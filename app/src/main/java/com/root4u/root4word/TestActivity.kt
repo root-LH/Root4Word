ca826abf3db3666c3903560b61e3d80a94b74644
Glaysia <harry261@naver.com>
2022-12-27 22:27:16 +0900

    Hello My Name Is Kimberly I Added some picture and test layout

---
+++ app/src/main/java/com/root4u/root4word/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.root4u.root4word
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test)
+    }
+}
